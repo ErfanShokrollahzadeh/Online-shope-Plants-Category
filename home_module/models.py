1280f091acaf7e4359148d8b43e8afbf53260334
ERFAN <erfan.shokr300@gmail.com>
2024-12-28 20:44:36 +0300

    Update models to allow optional images; enhance homepage with new navigation links and default images

--- home_module/models.py
+++ home_module/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class HeroSection(models.Model):
     title = models.CharField(max_length=200)
     subtitle = models.TextField()
-    hero_image = models.ImageField(upload_to='hero/')
+    hero_image = models.ImageField(upload_to='hero/', blank=True, null=True)
     
     def __str__(self):
         return self.title
@@ -12,7 +12,7 @@ class PlantBenefit(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     icon_class = models.CharField(max_length=50, help_text="Bootstrap icon class (e.g., bi-tree)")
-    image = models.ImageField(upload_to='benefits/')
+    image = models.ImageField(upload_to='benefits/', blank=True, null=True)
     
     def __str__(self):
         return self.title
--- home_module/views.py
+++ home_module/views.py
@@ -8,7 +8,14 @@ from .models import HeroSection, PlantBenefit
 def indexhome(request):
     hero = HeroSection.objects.first()
     benefits = PlantBenefit.objects.all()
+    
+    # Default images
+    default_hero_image = 'https://source.unsplash.com/random/1600x900/?plants'
+    default_benefit_image = 'https://source.unsplash.com/random/400x300/?plant'
+    
     return render(request, 'home_module/index.html', {
         'hero': hero,
         'benefits': benefits,
+        'default_hero_image': default_hero_image,
+        'default_benefit_image': default_benefit_image,
     })
